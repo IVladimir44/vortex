6974c2c4f07ad182bf6d7183574282ebf431eb9b
IVladimir44 <dpoleeff97@gmail.com>
2022-01-24 17:37:39 +0200

    начал делать страницу с сообщениями

--- src/components/pages/Messages.vue
+++ src/components/pages/Messages.vue
@@ -1,7 +1,17 @@
 <template>
-<div class="container">
-    <div class="wrapper">
+<div class="background">
+    <div class="title_wrapper">
         <p class="messages_title">Сообщения</p>
+    </div>
+    <div class="messages_wrapper">
+        <div class="messages">
+            <ul class="messages_list">
+                <li class="message">Сообщение</li>
+                <li class="message">Сообщение</li>
+                <li class="message">Сообщение</li>
+                <li class="message">Сообщение</li>
+            </ul>
+        </div>
     </div>
 </div>
 </template>
@@ -11,8 +21,42 @@
 </script>
 
 <style scoped>
-.container {
-    background-color: #237aa5;
-    margin: 0 0 0 0;
+.background {
+    background-color: #F2F4F5;
+    padding-bottom: 40px;
+}
+.title_wrapper {
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    max-width: 1238px;
+    background-color: #FFFF;
+    border-radius: 6px;
+}
+.messages_wrapper {
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    max-width: 1238px;
+}
+.messages_title {
+    padding-top: 55px;
+    padding-bottom: 30px;
+    margin-left: 46px;
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 1;
+    color: #002f34;
+}
+.messages_list {
+list-style-type: none;
+margin: 40px 16px 0 16px;
+padding: 0;
+}
+.message {
+    width: 1206px;
+    height: 38px;
+    padding: 16px;
+    background-color: #FFFF;
 }
 </style>
